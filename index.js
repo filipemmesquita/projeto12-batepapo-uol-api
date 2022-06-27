294f7dbfd16159d1475b3edc3f6d52ee17f9e812
Filipe Mesquita <filipemmesquita95@gmail.com>
2022-06-27 13:51:20 -0300

    rota get/participants implementada 	modified:   index.js

--- index.js
+++ index.js
@@ -45,6 +45,19 @@ app.post('/participants', async (req, res) => {
     }
 });
 
+app.get('/participants', async (req, res) => {
+    try{
+    const participants=await db.collection('participants').find().toArray();
+    if (!participants) {
+        return res.sendStatus(404);
+    }
+    res.send(participants);
+    }catch (error) {
+        console.error(error);
+        res.sendStatus(500);
+    }
+});
+
 app.listen(5000, () => {
     console.log('Server is litening on port 5000.');
 });
